8bc522ee5018d7b354cf745823d2da0d649d6d3e
m.pielaszkiewicz <m.pielaszkiewicz@komputronik.pl>
2022-09-04 21:42:21 +0200

    db migration to new structure

--- db/scripts/new/db_model.sql
+++ db/scripts/new/db_model.sql
@@ -31,7 +31,7 @@ CREATE TABLE `pilots_statuses` (
 );
 
 CREATE TABLE `squadron_planes` (
-                                   `squadron_plane_id` int PRIMARY KEY,
+                                   `squadron_plane_id` int PRIMARY KEY AUTO_INCREMENT,
                                    `squadron_plane_type` int NOT NULL,
                                    `squadron_plane_status` int NOT NULL
 );
@@ -50,7 +50,8 @@ CREATE TABLE `pilot_missions` (
                                   `id` int PRIMARY KEY AUTO_INCREMENT,
                                   `squadron_mission_id` int NOT NULL,
                                   `squadron_pilot_id` int NOT NULL,
-                                  `pilot_mission_role` int NOT NULL,
+                                  `pilot_mission_flight_role` int NOT NULL,
+                                  `pilot_mission_flight_type` int NOT NULL,
                                   `pilot_mission_kill_aa_pvp` int,
                                   `pilot_mission_kill_aa_ai` int,
                                   `pilot_mission_kill_ag` int,
@@ -59,6 +60,17 @@ CREATE TABLE `pilot_missions` (
                                   `pilot_mission_kill_ff` int,
                                   `pilot_mission_time` int NOT NULL,
                                   `pilot_mission_status` int NOT NULL
+);
+
+CREATE TABLE `pilot_mission_options` (
+                                         `id` int PRIMARY KEY AUTO_INCREMENT,
+                                         `pilot_mission_id` int NOT NULL,
+                                         `pilot_mission_option_id` int NOT NULL
+);
+
+CREATE TABLE `pilot_mission_options_types` (
+                                               `pilot_mission_options_type_id` int PRIMARY KEY AUTO_INCREMENT,
+                                               `pilot_mission_options_type_value` varchar(255) UNIQUE NOT NULL
 );
 
 CREATE TABLE `squadron_missions` (
@@ -80,9 +92,14 @@ CREATE TABLE `squadron_missions_details` (
                                              `missions_detail_value` varchar(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE `mission_roles` (
-                                 `mission_role_id` int PRIMARY KEY AUTO_INCREMENT,
-                                 `mission_role_value` varchar(64)
+CREATE TABLE `pilot_mission_flight_roles` (
+                                              `pilot_mission_flight_role_id` int PRIMARY KEY AUTO_INCREMENT,
+                                              `pilot_mission_flight_role_value` varchar(64)
+);
+
+CREATE TABLE `pilot_mission_flight_types` (
+                                              `pilot_mission_flight_type_id` int PRIMARY KEY AUTO_INCREMENT,
+                                              `pilot_mission_flight_type_value` varchar(64)
 );
 
 CREATE TABLE `mission_statuses` (
@@ -124,11 +141,17 @@ ALTER TABLE `squadron_planes` ADD CONSTRAINT `plane_status` FOREIGN KEY (`squadr
 
 ALTER TABLE `squadron_planes` ADD CONSTRAINT `plane_type` FOREIGN KEY (`squadron_plane_type`) REFERENCES `plane_types` (`plane_type_id`);
 
-ALTER TABLE `pilot_missions` ADD CONSTRAINT `mission_roles` FOREIGN KEY (`pilot_mission_role`) REFERENCES `mission_roles` (`mission_role_id`);
+ALTER TABLE `pilot_missions` ADD CONSTRAINT `mission_roles` FOREIGN KEY (`pilot_mission_flight_role`) REFERENCES `pilot_mission_flight_roles` (`pilot_mission_flight_role_id`);
+
+ALTER TABLE `pilot_missions` ADD CONSTRAINT `mission_types` FOREIGN KEY (`pilot_mission_flight_type`) REFERENCES `pilot_mission_flight_types` (`pilot_mission_flight_type_id`);
+
+ALTER TABLE `pilot_mission_options` ADD CONSTRAINT `mission_options` FOREIGN KEY (`pilot_mission_id`) REFERENCES `pilot_missions` (`id`);
 
 ALTER TABLE `pilot_missions` ADD CONSTRAINT `mission_status` FOREIGN KEY (`pilot_mission_status`) REFERENCES `mission_statuses` (`mission_status_id`);
 
 ALTER TABLE `pilot_missions` ADD CONSTRAINT `squadron_mission` FOREIGN KEY (`squadron_mission_id`) REFERENCES `squadron_missions` (`squadron_mission_id`);
+
+ALTER TABLE `pilot_mission_options` ADD CONSTRAINT `option_type` FOREIGN KEY (`pilot_mission_option_id`) REFERENCES `pilot_mission_options_types` (`pilot_mission_options_type_id`);
 
 ALTER TABLE `squadron_missions` ADD CONSTRAINT `mission_type` FOREIGN KEY (`squadron_mission_type`) REFERENCES `squadron_missions_types` (`missions_type_id`);
 
@@ -137,32 +160,41 @@ ALTER TABLE `squadron_missions` ADD CONSTRAINT `mission_details` FOREIGN KEY (`s
 ALTER TABLE `bulletin_graphics` ADD CONSTRAINT `bulletin_graphic_uri` FOREIGN KEY (`bulletin_id`) REFERENCES `bulletins` (`bulletin_id`);
 
 ALTER TABLE `bulletin_graphics` ADD CONSTRAINT `graphic_uri` FOREIGN KEY (`graphic_uri_id`) REFERENCES `graphics_uri` (`graphic_uri_id`);
-
 INSERT INTO plane_types (plane_type_id, plane_type_value) VALUES (1, 'F/A-18C'), (2, 'AV-8B');
 
-INSERT INTO mission_roles (mission_role_id, mission_role_value)
-VALUES  (1, 'TARCAP'),
-        (2, 'Rebasing'),
-        (3, 'Training'),
-        (4, 'SEAD'),
-        (5, 'Ground Attack'),
-        (6, 'BARCAP'),
-        (12, 'Test'),
-        (13, 'AIC'),
-        (14, 'Close Escort/Sweep'),
-        (17, 'Self Escort SEAD/Strike'),
-        (18, 'CAP'),
-        (19, 'GCI'),
-        (20, 'Deep strike'),
-        (21, 'Close escort'),
-        (22, 'AntiShip Strike'),
-        (23, 'Kadet'),
-        (24, 'Instruktor'),
-        (25, ''),
-        (26, 'Lead'),
-        (27, 'D/C'),
-        (28, 'Flight lead'),
-        (29, 'A/GIC');
+INSERT INTO pilot_mission_flight_roles (pilot_mission_flight_role_id, pilot_mission_flight_role_value)
+VALUES  (1, 'LEAD'),
+        (2, 'WINGMAN'),
+        (3, 'DIVISION LEAD'),
+        (4, 'SINGLETON'),
+        (5, 'ATC/MAGIC'),
+        (6, 'FAC/JTAC'),
+        (7, 'LSO/AIRBOSS');
+
+INSERT INTO pilot_mission_flight_types (pilot_mission_flight_type_id, pilot_mission_flight_type_value)
+VALUES
+        (1, 'SEAD'),
+        (2, 'DEAD'),
+        (3, 'STRIKE'),
+        (4, 'ESCORT'),
+        (5, 'CAS'),
+        (6, 'CAP'),
+        (7, 'INTERCEPT'),
+        (8, 'RECON'),
+        (9, 'OTHER');
+
+INSERT INTO pilot_mission_options_types (pilot_mission_options_type_id, pilot_mission_options_type_value)
+VALUES
+    (1, 'AAR'),
+    (2, 'CASE I'),
+    (3, 'CASE II'),
+    (4, 'CASE III'),
+    (5, 'NIGHT'),
+    (6, 'BFM'),
+    (7, 'ACM'),
+    (8, 'STANDOFF'),
+    (9, 'LASER'),
+    (10, 'UNGUIDED');
 
 INSERT INTO pilots_statuses (status_id, status_value)
 VALUES  (1, 'CO'),
@@ -171,16 +203,17 @@ VALUES  (1, 'CO'),
         (4, 'CADET'),
         (5, 'ExWombat');
 
+INSERT INTO squadron_missions_types (missions_type_id, missions_type_value)
+VALUES (1, 'HRP'),
+       (2, 'LRP'),
+       (3, 'EXAMS'),
+       (4, 'FREE TRAINING'),
+       (5, 'SQUADRON TRAINING'),
+       (6, 'CQ'),
+       (7, 'OTH'),
+       (8, 'JOINT OPS');
+
 INSERT INTO plane_statuses (plane_status_id, plane_status_value)
 VALUES  (1, 'IN SERVICE'),
         (2, 'MAINTENANCE'),
         (3, 'RETIRED FROM SERVICE');
-
-INSERT INTO squadron_missions_types (missions_type_id, missions_type_value)
-VALUES (1, 'COMBAT'),
-       (2, 'TRAINING'),
-       (3, 'AIDED TRAINING'),
-       (4, 'LRP'),
-       (5, 'OTH'),
-       (6, 'EXAMS'),
-       (7, 'CQ')
